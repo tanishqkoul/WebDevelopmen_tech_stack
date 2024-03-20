b24fffed15b34199e8c2993421ebf47879c38269
Tanishq <koul.tanishq111@gmail.com>
2024-03-20 23:08:30 +0530

    intro to java script

---
+++ WebDev/day54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Code is running...")
+console.log("Code is also running  very fast. It means you are a great developer...")
+console.log("Code is looking like a wow...")
--- WebDev/day54/script.js
+++ WebDev/day54/script.js
@@ -1,1 +1,23 @@
-This is script.js in WebDev/day54
+alert("hello world");
+console.log("code is running")
+console.log("application is developing")
+console.log("You are a great developer we are proud of you")
+
+
+var a = prompt("are you sure you want to continue")
+var isTrue = confirm("are you sure you want to leave this page or your laptop is going to blast")
+
+if (isTrue){
+    console.log("Computer is blasting")
+
+}
+else{
+    console.log("Computer is not blasting")
+
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+document.body.style.backgroundColor = "red"
