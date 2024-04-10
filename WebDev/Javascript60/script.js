ca638e87d7ebded211442eb648cb81c995e08793
Tanishq <koul.tanishq111@gmail.com>
2024-04-10 23:02:24 +0530

    strings in java script

---
+++ WebDev/Javascript60/script.js
@@ -0,0 +1,32 @@
+console.log("This is strung practice")
+
+a="Tanishq"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+// console.log(a[6]
+
+console.log(a.length)
+
+let my_name = "Tanishq"
+let Friend = "Suvansh"
+console.log("My name is " + my_name + " and my friend name is "  +  Friend)
+
+console.log(`my name is ${my_name} and my friend name is ${Friend}`)
+
+let  b = "Saksham"
+c = "Anchal"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+console.log(b.replace("sh", "Topu"))
+let d = c.concat ("Love", a)
+
+console.log(b)
+console.log(d)
