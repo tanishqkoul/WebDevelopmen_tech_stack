815357904df8429c52a08cc37fbf40129eb03139
Tanishq <koul.tanishq111@gmail.com>
2024-04-20 22:18:11 +0530

    event listeners

--- WebDev/Javascript74/script.js
+++ WebDev/Javascript74/script.js
@@ -1,1 +1,15 @@
-This is script.js in WebDev/day74
+let button = document.getElementById("btn")
+// list of all mouse events
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+button.addEventListener("dblclick",()=>{
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked Enjot YOur click Tanishq. You are a great doveloper . you are a billonare</b>"
+})
+
+button.addEventListener(contextmenu,()=>{
+    alert("Dont try to hack us by right click")
+})
+
+button.addEventListener(keyDown,(e)=>{
+   console.log(e, e.key, e.keyCode)
+})
