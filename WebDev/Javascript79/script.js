657488a8bf72c62219cd552d6ddf7b50b5ec4a96
Tanishq <koul.tanishq111@gmail.com>
2024-05-05 16:44:01 +0530

    error handelling

--- WebDev/Javascript79/script.js
+++ WebDev/Javascript79/script.js
@@ -1,1 +1,28 @@
-This is script.js in WebDev/day79
+let a = prompt("Enter first number :")
+
+let b = prompt("Enter second number :")
+
+if (isNaN(a) || isNaN(b)){
+
+    throw SyntaxError("Sorry Java script does not allow this")
+}
+
+let sum = parseInt(a) + parseInt(b)
+
+function main(){
+
+    let x = 1;
+    try {
+        console.lo("The sum is " , sum *x)
+        return true
+    }
+    catch(err){
+        console.log("Mere bhai error ah gaya . Dakh le syntax hoga pakka")
+        return false
+    }
+    finally{
+        console.log("all the files will be closed shortly")
+    }
+}
+
+let c = main()
