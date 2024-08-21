4fb347ac530b3a3d4a9667348ba9141e15add964
Tanishq <koul.tanishq111@gmail.com>
2024-08-22 00:29:08 +0530

    backend

--- WebDev/day86/script.js
+++ WebDev/day86/script.js
@@ -14,3 +14,18 @@
 const a = require("./mymodule2.js")
 
 console.log(a, __dirname, __filename)
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello anchal!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+// nodemon script.js for autoupdate
+// node ./script.js for satrting server
