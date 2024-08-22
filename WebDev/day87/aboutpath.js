7dd5fd6746970160f5be13988280910a8c7b7aef
Tanishq <koul.tanishq111@gmail.com>
2024-08-23 00:23:06 +0530

    changes

---
+++ WebDev/day87/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "C:\\Users\\iitia\\Downloads\\Sigma Web Development Course\\Sigma-W"
+
+console.log(path.extname(myPath))
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/", "programs\\Tanishq.txt"))
