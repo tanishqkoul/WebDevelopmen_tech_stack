34674c9836dac2230afa3ca84ac123c177c24a96
Tanishq <koul.tanishq111@gmail.com>
2024-10-03 22:25:04 +0530

    mongoose

--- WebDev/day96/script.js
+++ WebDev/day96/script.js
@@ -1,19 +1,27 @@
-import mongoose from 'mongoose';
+// https://www.npmjs.com/package/mongodb
+import mongoose from "mongoose";
 import express from "express";
-import {Todo} from './models/Todo.js';
+import { Todo } from "./models/Todo.js";
 
 let conn = await mongoose.connect("mongodb://localhost:27017/todo")
-
-// const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    const todo = new Todo({title : "Hey first todo" , desc : 'write the discription here', isDone : false})
-    todo.save()
-  res.send('Hello World!')
+
+    const todo = new Todo({  desc: "Description of this todo", isDone: false, days: Math.floor(Math.random() * 45 + 5* Math.random()) })
+    todo.save() 
+    res.send('Hello World!')
 })
 
+app.get('/a', async (req, res) => { 
+    let todo = await Todo.findOne({})
+    console.log(todo)
+    res.json({title: todo.title, desc: todo.desc})
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
