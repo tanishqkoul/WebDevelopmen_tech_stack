2323973446c777f2710090996343d7c892b8e0ca
Tanishq <koul.tanishq111@gmail.com>
2024-10-03 22:12:31 +0530

    moongose

--- WebDev/day96/script.js
+++ WebDev/day96/script.js
@@ -1,1 +1,19 @@
-This is script.js in WebDev/day96
+import mongoose from 'mongoose';
+import express from "express";
+import {Todo} from './models/Todo.js';
+
+let conn = await mongoose.connect("mongodb://localhost:27017/todo")
+
+// const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const todo = new Todo({title : "Hey first todo" , desc : 'write the discription here', isDone : false})
+    todo.save()
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
